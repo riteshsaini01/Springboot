eb204d2ea3f55beed70ff04a73a6b831807cedf6
riteshsaini01 <b210885@skit.ac.in>
2024-09-21 11:25:13 +0530

    Adding first basic project of springboot

---
+++ springprojectfirst/src/main/java/com/firstspringproject/springprojectfirst/SpringprojectfirstApplication.java
@@ -0,0 +1,13 @@
+package com.firstspringproject.springprojectfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectfirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectfirstApplication.class, args);
+	}
+
+}
---
+++ springprojectfirst/src/test/java/com/firstspringproject/springprojectfirst/SpringprojectfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringproject.springprojectfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
