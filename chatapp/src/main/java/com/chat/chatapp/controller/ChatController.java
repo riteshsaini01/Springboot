a9f52464097adbcb79a3e398fc4180078ff2568d
riteshsaini01 <b210885@skit.ac.in>
2025-12-07 13:47:51 +0530

    Added websocket functionalities

---
+++ chatapp/src/main/java/com/chat/chatapp/controller/ChatController.java
@@ -0,0 +1,10 @@
+package com.chat.chatapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    public ChatMessage sendMessage(ChatMessage message){
+        
+    }
+}
